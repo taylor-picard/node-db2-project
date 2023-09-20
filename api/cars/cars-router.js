3fcf97dbc35f742f0e1802a2ddd9e73c4a828035
Taylor Picard <taylorkpicard1998@gmail.com>
2023-09-20 16:49:20 -0500

    Schema & models updated

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,27 @@
-// DO YOUR MAGIC
+const express = require('express');
+const Cars = require('./cars-model');
+const router = express.Router();
+
+router.get('/', async (req, res, next)=> {
+    try {
+        const cars = await Cars.getAll();
+        res.json(cars);
+    } catch (err) {
+        next(err);
+    }
+})
+
+router.get('/:id', async (req, res, next)=> {
+    try {
+        const car = await Cars.getById(req.params.id);
+        res.json(car);
+    } catch (err) {
+        next(err);
+    }
+})
+
+router.post('/', async (req, res, next)=> {
+    res.json('post new car');
+})
+
+module.exports = router;
